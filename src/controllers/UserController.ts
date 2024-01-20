5ac8d74ed4a50391ef7adb48b13cb91539e80103
luanmas <luanmas27@gmail.com>
2024-01-20 14:16:05 -0300

    generating authentication token and validating

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
-// import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import { userRepository } from '../repositories/userRepository';
 import { createUser } from '../models/userModel';
 
@@ -8,9 +8,9 @@ import { createUser } from '../models/userModel';
 export const UserController = {
     create: async (req : Request, res: Response) => {
         const { name, email, password } = req.body;
-        const userExist = await userRepository.getUser(email);
+        const user = await userRepository.getUser(email);
         
-        if(userExist.email) return res.status(400).json({ message: "Email já registrado!" })
+        if(user) return res.status(400).json({ message: "Email já registrado!" })
 
         const hashPassword = await bcrypt.hash(password, 10);
 
@@ -21,6 +21,29 @@ export const UserController = {
             name,
             email
         })
+    },
+
+    login: async (req : Request, res: Response) => {
+        const { email, password } = req.body;
+
+        const user = await userRepository.getUser(email);
+        
+        console.log("aaaaaaaaaa", user);
+        console.log("bbbbbbbbbb", password);
+        if(!user) return res.status(400).json({ message: "Email ou senha inválidos" })
+
+        const verifyPass = await bcrypt.compare(password, user.password);
+
+        if(!verifyPass) res.status(400).json({ message: "Email ou senha inválida" });
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_PASS, { expiresIn: '8h' });
+
+        const { password:_, ...userLogin } = user;
+
+        return res.json({
+            token,
+            user: userLogin
+        });
     }
 }
 
--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,15 +1,17 @@
 import { connection } from '../database/connection';
 import { RowDataPacket } from 'mysql2';
+import { IUser } from '../types/user';
 
 export const userRepository = {
-    getUser: async (email : string): Promise<{ email: boolean }> => {
-        const query = 'SELECT email FROM users WHERE email = ?';
+    getUser: async (email : string): Promise<IUser | undefined> => {
+        const query = 'SELECT username, email, user_id, password FROM users WHERE email = ?';
         const [rows] = await connection.query<RowDataPacket[]>(query, [email]);
     
         if(rows.length > 0) {
-            return { email : true }
+            const { email, username, user_id, password } = rows[0]; 
+            return { email, name: username, id: user_id, password };
         }
 
-        return { email : false };
+        return undefined;
     }
 }
