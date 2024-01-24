35d0a79fd40f8afe8a65ed4f6e57ce509a53dc34
luanmas <luanmas27@gmail.com>
2024-01-24 00:56:51 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from 'dotenv';
 import route from "./routes/routes";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/", route);
